346b7475ac5491fbc9a50ec29d40e114b9300e56
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-04-18 15:38:37 +0300

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Creates a MySQL test database, test db and test user for the project
+-- Bring privileges to user but only on this db
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
