7f0213f9df397139d87ab8e1f25c3d75a77e9d6e
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-04-21 05:31:55 +0300

    Test City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,20 +5,26 @@ from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Test class City """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialize tests """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """ Test state id """
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertEqual(str(new.__class__.__name__), "City")
 
-    def test_name(self):
-        """ """
+    def test_table_name(self):
+        """ Test name """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(new.__tablename__, "cities")
+
+    def test_state_id(self):
+        """ Test state id """
+        new = self.value()
+        new.state_id = "1"
+        self.assertEqual(new.state_id, "1")
