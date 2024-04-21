2d5a5e99b74ef077b137ae583187704b53c56196
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-04-21 05:33:02 +0300

    Test Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,15 +5,16 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Unittests for class Amenity"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Intialize test """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ Tests name """
         new = self.value()
+        new.name = "hello"
         self.assertEqual(type(new.name), str)
