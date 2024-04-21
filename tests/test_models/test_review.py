471725d0530cb441291ddbe2c39629303d7cd9a5
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-04-21 05:30:57 +0300

    Test Review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,25 +5,28 @@ from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ Test class Review """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialize test """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ Test place_id """
         new = self.value()
+        new.place_id = "2"
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Test user_id """
         new = self.value()
+        new.user_id = "testing"
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ Test text """
         new = self.value()
+        new.text = "hello"
         self.assertEqual(type(new.text), str)
