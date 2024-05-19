a07bacfe9acb9182d5133a78defc660802588aba
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-05-19 22:38:51 +0300

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-# Starts a Flask web application
+""" Starts a Flask web application """
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """ Returns Hello HBNB! """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """ Returns HBNB """
     return 'HBNB'
 
 
